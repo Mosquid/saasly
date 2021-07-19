52da9838a4dc39224017e9070ea6f9f8b1400967
Dmitry Mosquid <mosquid@gmail.com>
2021-07-19 20:19:56 +0300

    golden api url fix

--- golden.js
+++ golden.js
@@ -1,75 +1,75 @@
-const { fetchPageData } = require("./http")
-const { isListed, addToUsedList } = require("./used")
+const { fetchPageData } = require("./http");
+const { isListed, addToUsedList } = require("./used");
 const apiUrl =
-  "https://golden.com/api/v1/queries/list-of-software-as-a-service-companies/results/"
+  "https://golden.com/api/v1/queries/list-of-software-as-a-service-companies/results?is_public=1";
 //?page=1&per_page=100
 
-const minPage = 1
-const maxPage = 20
-const perPage = 10
+const minPage = 1;
+const maxPage = 30;
+const perPage = 10;
 
 const getDataOffset = () => {
-  const page = Math.random() * (maxPage - minPage) + minPage
-  const post = Math.random() * (perPage - minPage) + minPage
+  const page = Math.random() * (maxPage - minPage) + minPage;
+  const post = Math.random() * (perPage - minPage) + minPage;
 
   if (isListed(post * page)) {
-    return getDataOffset()
+    return getDataOffset();
   }
-  return [page, post].map(Math.round)
-}
+  return [page, post].map(Math.round);
+};
 
 const parseSiteData = (data) => {
-  const { name, thumbnail, description, generated_description } = data
-  const icon = thumbnail.original || ""
+  const { name, thumbnail, description, generated_description } = data;
+  const icon = thumbnail.original || "";
 
   const info = collectLeafs({
     nodes: [].concat(generated_description.nodes, description.nodes),
   })
     .join(" ")
-    .trim()
+    .trim();
 
   return {
     name,
     info,
     icon,
-  }
-}
+  };
+};
 
 const collectLeafs = (desc) => {
   try {
-    const { nodes } = desc
+    const { nodes } = desc;
 
     return nodes.map((node) => {
       if (node.leaves) {
         return node.leaves
           .map(({ text }) => text)
           .filter(Boolean)
-          .join(" ")
+          .join(" ");
       }
-      return collectLeafs(node)
-    })
+      return collectLeafs(node);
+    });
   } catch (error) {
-    console.error(error)
-    return null
+    console.error(error);
+    return null;
   }
-}
+};
 
 const getSaasData = async () => {
   try {
-    const [page, post] = getDataOffset()
-    const url = `${apiUrl}?page=${page}&per_page=${perPage}`
-    const data = await fetchPageData(url)
-    const { results } = data
-    const saas = results[post - 1]
+    const [page, post] = getDataOffset();
+    const url = `${apiUrl}&page=${page}&per_page=${perPage}`;
+    const data = await fetchPageData(url);
+    const { results } = data;
+    const saas = results[post - 1];
 
-    addToUsedList(page * post)
+    addToUsedList(page * post);
 
-    return parseSiteData(saas)
+    return parseSiteData(saas);
   } catch (error) {
-    console.error(error)
-    return null
+    console.error(error);
+    return null;
   }
-}
+};
 
-module.exports.getDataOffset = getDataOffset
-module.exports.getSaasData = getSaasData
+module.exports.getDataOffset = getDataOffset;
+module.exports.getSaasData = getSaasData;
