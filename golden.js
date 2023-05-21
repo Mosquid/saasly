b1d979156bfd65ef52b3bb80740ab88c92c43f04
Dmitry Mosquid <mosquid@gmail.com>
2023-05-21 17:00:10 +0300

    company info request added

--- golden.js
+++ golden.js
@@ -1,7 +1,6 @@
 const { fetchPageData } = require("./http");
 const { isListed, addToUsedList } = require("./used");
-const apiUrl =
-  "https://golden.com/api/v1/queries/list-of-software-as-a-service-companies/results?is_public=1";
+const apiUrl = "https://golden.com/api/v1/";
 //?page=1&per_page=100
 
 const minPage = 1;
@@ -19,7 +18,9 @@ const getDataOffset = () => {
 };
 
 const parseSiteData = (data) => {
-  const { name, thumbnail, description, generated_description } = data;
+  if (!data) return null;
+
+  const { name, thumbnail, description, generated_description = {} } = data;
   const icon = thumbnail.original || "";
 
   const info = collectLeafs({
@@ -35,12 +36,12 @@ const parseSiteData = (data) => {
   };
 };
 
-const collectLeafs = (desc) => {
+const collectLeafs = (desc = {}) => {
   try {
-    const { nodes } = desc;
+    const { nodes = [] } = desc;
 
     return nodes.map((node) => {
-      if (node.leaves) {
+      if (node && node.leaves) {
         return node.leaves
           .map(({ text }) => text)
           .filter(Boolean)
@@ -57,14 +58,28 @@ const collectLeafs = (desc) => {
 const getSaasData = async () => {
   try {
     const [page, post] = getDataOffset();
-    const url = `${apiUrl}&page=${page}&per_page=${perPage}`;
+    const url = `${apiUrl}queries/list-of-software-as-a-service-companies-BMY/results?page=${page}&per_page=${perPage}`;
     const data = await fetchPageData(url);
     const { results } = data;
     const saas = results[post - 1];
 
     addToUsedList(page * post);
 
-    return parseSiteData(saas);
+    return getCompanyDetails(saas.slug);
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+const getCompanyDetails = async (cid) => {
+  try {
+    const url = `${apiUrl}entities/${cid}/?quick_view=true`;
+    const data = await fetchPageData(url);
+    const { structuredData } = data;
+
+    if (!structuredData) return null;
+    return parseSiteData(structuredData);
   } catch (error) {
     console.error(error);
     return null;
