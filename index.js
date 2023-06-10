31cf98eb266220804c141eaebaae143d9e1828c7
Dmitry Mosquid <mosquid@gmail.com>
2023-06-10 08:47:12 +0300

    simple retry added

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, ".env") });
 const { getSaasData } = require("./golden");
 const { Telegraf } = require("telegraf");
+let attempts = 5;
 
 async function main() {
-  const data = await getSaasData();
-  const { name, info, icon } = data;
-  const bot = new Telegraf(process.env.BOT_TOKEN);
-  const msg = `<b>${name}</b>\r\n\r\n${info}`;
+  try {
+    const data = await getSaasData();
+    const { name, info, icon } = data;
+    const bot = new Telegraf(process.env.BOT_TOKEN);
+    const msg = `<b>${name}</b>\r\n\r\n${info}`;
 
-  bot.telegram.sendPhoto(process.env.CHANNEL_NAME, icon, {
-    caption: msg,
-    parse_mode: "HTML",
-  });
+    bot.telegram.sendPhoto(process.env.CHANNEL_NAME, icon, {
+      caption: msg,
+      parse_mode: "HTML",
+    });
+  } catch (error) {
+    console.error(error);
+    if (attempts > 0) {
+      attempts--;
+      main();
+    }
+  }
 }
 
 main();
